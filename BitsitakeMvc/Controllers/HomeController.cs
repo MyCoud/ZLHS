a65d0ebf49aee49dc9f7a3f1f25b60d3368d34d6
PC <PC@YLS>
2021-02-23 10:55:34 +0800

    13

--- BitsitakeMvc/Controllers/HomeController.cs
+++ BitsitakeMvc/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace BitsitakeMvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        //测试1
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
